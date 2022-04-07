a05c8a7b788fc1563c30569e21f184b3e989d85d
Aleksandr Goncharov <cooperjkr@ya.ru>
2022-04-07 11:07:24 +0500

    C#. Приведение типов, метод Object.GetType()

---
+++ TypeConversions/Program.cs
@@ -0,0 +1,27 @@
+﻿object manager = new Manager();
+// Object.GetType() - это невиртуальный метод, который возвращает тип,
+// содержащийся в переменной
+Console.WriteLine(manager.GetType().Name);
+PromoteEmployee(manager);
+
+object dateTime = new DateTime(2022, 1, 1);
+Console.WriteLine(dateTime.GetType().Name);
+// Исключение InvalidCastException
+PromoteEmployee(dateTime);
+
+// Если object поменяем на Employee, то получим ошибку на этапе компиляции
+static void PromoteEmployee(object o)
+{
+    // Можем использовать оператор is или as, чтобы избежать исключения, но
+    // as в результате будет производительнее, чем is
+    Employee e = (Employee)o;
+}
+
+internal class Employee
+{
+
+}
+internal class Manager : Employee
+{
+
+}
